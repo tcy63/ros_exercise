aa0a3af195a06a070b3771a09ae7d046df51e6f1
tcy63 <2000017814@stu.pku.edu.cn>
2023-02-15 20:08:34 +0000

    revise the simple_subscriber.py for question 2

--- simple_subscriber.py
+++ simple_subscriber.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
+"""
+Description:
+    Subscribes to the topic published on by the simple_publisher and publishes the natural log of the received messages.
+"""
+import math
 
 import rospy
 from std_msgs.msg import Float32
 
+pub = rospy.Publisher("random_float_log", Float32, queue_size=10)
+
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    pub.publish(math.log(data.data))
 
 def listener():
     rospy.init_node('simple_subscriber')
